0b3a7d9f7f2045d026790adf0fa61a287e611a42
Brian Beloshapka <bbelos@hotmail.com>
2014-11-02 00:04:18 -0400

    sero8: gpu blobs

--- sero8-vendor-blobs.mk
+++ sero8-vendor-blobs.mk
@@ -13,3 +13,24 @@
 # limitations under the License.
 
 LOCAL_PATH := vendor/hisense/sero8
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/lib/libMali.so:system/lib/libMali.so \
+    $(LOCAL_PATH)/proprietary/lib/libUMP.so:system/lib/libUMP.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
+    $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/lib/modules/mali.ko.3.0.36+:system/lib/modules/mali.ko.3.0.36+ \
+    $(LOCAL_PATH)/proprietary/lib/modules/mali.ko:system/lib/modules/mali.ko \
+    $(LOCAL_PATH)/proprietary/lib/modules/ump.ko.3.0.36+:system/lib/modules/ump.ko.3.0.36+ \
+    $(LOCAL_PATH)/proprietary/lib/modules/ump.ko:system/lib/modules/ump.ko
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/etc/performance_info.xml:system/etc/performance_info.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/lib/hw/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so \
+    $(LOCAL_PATH)/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so \
+    $(LOCAL_PATH)/proprietary/lib/libion.so:system/lib/libion.so
